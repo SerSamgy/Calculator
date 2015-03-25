f591835b98bcd6ea11921f60afb7e1bed97f6753
samgy <samgy-cool@yandex.ru>
2015-03-25 19:09:12 +0300

    Change model and serializer

--- main_project/calculator/models.py
+++ main_project/calculator/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
 class Solver(models.Model):
-    cur_val = models.IntegerField(default=0)
-    res_val = models.IntegerField(default=0)
-    sign = models.CharField(max_length=1, blank=True, default=None)
-    result = models.CharField(max_length=10, blank=True, default='0')
+    expression = models.CharField(max_length=20, blank=True, default='0')
+    result = models.CharField(max_length=20, blank=True, default='0')
--- main_project/calculator/serializers.py
+++ main_project/calculator/serializers.py
@@ -4,7 +4,7 @@ from models import Solver
 class SolverSerializer(serializers.ModelSerializer):
     class Meta:
         model = Solver
-        fields = ('cur_val', 'res_val', 'sign', 'result')
+        fields = ('expression', 'result')
 
     def create(self, validated_data):
         """
@@ -16,9 +16,7 @@ class SolverSerializer(serializers.ModelSerializer):
         """
         Update and return an existing `Solver` instance, given the validated data.
         """
-        instance.cur_val = validated_data.get('cur_val', instance.cur_val)
-        instance.res_val = validated_data.get('res_val', instance.res_val)
-        instance.sign = validated_data.get('sign', instance.sign)
+        instance.expression = validated_data.get('expression', instance.expression)
         instance.result = validated_data.get('result', instance.result)
         instance.save()
         return instance
